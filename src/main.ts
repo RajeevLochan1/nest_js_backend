a61457bcf15934510631d72924a99829e6785a20
RajeevLochan1 <rajeevlochan623@gmail.com>
2024-02-20 09:07:10 +0530

    added frontend url in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+    // Enable CORS for a specific frontend URL
+    app.enableCors({
+      origin: 'https://add-bookmarks.netlify.app/',
+      credentials: true, // if your frontend sends credentials (cookies, headers), set this to true
+    });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
