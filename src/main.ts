145e4a4c3c57941e6e40e10c4741b4edb858557e
Rajeev Lochan Panda <95914311+RajeevLochan1@users.noreply.github.com>
2024-05-18 11:34:24 +0530

    Added vercel link of frontend
    Added vercel link of frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   // Enable CORS for a specific frontend URL
   app.enableCors({
-    origin: 'https://add-bookmarks.netlify.app/',
+    origin: 'https://nest-js-frontend.vercel.app/',
     credentials: true, // if your frontend sends credentials (cookies, headers), set this to true
   });
   app.useGlobalPipes(
